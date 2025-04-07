92aaa2a86c971316390d27cf06a2e0e45355c54c
Andrew1013-development <viethuytran147@gmail.com>
2025-04-07 10:48:10 +0700

    using JSON for configuration

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 COPY /app/blueprints/ blueprints/
 COPY /app/__init__.py __init__.py
 COPY /requirements.txt requirements.txt
-COPY /app/.env .env
 RUN pip3 install -r requirements.txt
 
 EXPOSE 5000
--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
-from os import environ
-from dotenv import load_dotenv
+from json import load as json_load
 from flask import Flask
 
 def create_app():
-    load_dotenv()
-
     app = Flask(__name__)
-    app.config["IMG_DIR"] = environ["IMG_DIR"]
-    app.config["VERSION"] = "v0.2.3"
+    app.config.from_file("config.json", load=json_load)
+    app.config["VERSION"] = "v0.2.4"
 
     from .blueprints import carouselpage, imagepage, homepage
     app.register_blueprint(carouselpage)
